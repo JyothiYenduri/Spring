cb76be446bdfa13730af6d8e39465c813f42d9ee
JyothiYenduri <120366268+JyothiYenduri@users.noreply.github.com>
2023-04-11 12:13:37 +0530

    Add files via upload

---
+++ Calculator/src/main/java/MathFunctions/MathService.java
@@ -0,0 +1,8 @@
+package MathFunctions;
+
+public interface MathService {
+	public int add(int a,int b);
+	public int sub(int a,int b);
+	public int mul(int a,int b);
+	public int div(int a,int b);
+}
